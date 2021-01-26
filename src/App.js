0d48bfdd151c6d77e532a5fba80958cb35334c79
James McWhorter <james3397@gmail.com>
2021-01-26 12:11:10 -0500

    Example 1 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -19,7 +20,26 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+}
+
+// Example 1: Default Parameters
+	console.log("Example 1: Default Parameters");
+var defaultStudent = {
+	name: {
+		firstName: "Bobby",
+		lastName: "Johnson" 
+	},
+	favSubject: "math"
+}
+function  myClass (x = defaultStudent){
+	console.log('Use default');
+	console.log('The first student in my class is ${x.name.firstName} ${x.name.lastName} and his favorite subject is ${x.favSubject}..');
+}
+function  myClass2 (name = "Gerald Hopkins", subject = "science"){
+	        console.log('Use parameters');
+	        console.log('The second student in my class is ${name} and his favorite subject is ${subject}..');
+		console.log("----------");
 }
 
 export default App;
